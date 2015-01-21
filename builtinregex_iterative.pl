01c222c8ac47b969c7100478b079c6d54931cdc6
tristan <tristan@ogedai.kgb.im>
2015-01-21 21:26:19 +1100

    results

--- builtinregex_iterative.pl
+++ builtinregex_iterative.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use Carp;
 use RegexTester;
+use Time::HiRes qw(gettimeofday tv_interval);
 
 my ($corpus,$page) = @ARGV;
 unless($corpus && $page) {
@@ -13,13 +14,18 @@ my $fh = RegexTester::openPath($corpus);
 my @regexs = RegexTester::fhToRegexpList($fh);
 my $pageData = RegexTester::fetchPage($page);
 
-TESTER: for (my $i = 0; $i < 100; $i++) {
+my ($total,$num);
+TESTER: for (my $i = 0; $i < 10; $i++) {
 	print STDERR localtime . ": test #$i\n";
+	my $start = [gettimeofday];
 	MATCHER: for(my $ri=0; $ri <= $#regexs; $ri++) {
 		if($pageData =~ $regexs[$ri]) {
 			warn "matched $regexs[$ri]";
 			last MATCHER;
 		}
 	}
+	my $delt = tv_interval ( $start, [gettimeofday]);
+	$total += $delt; $num++;
+	print STDERR "#$i took: $delt\n"; 
 }
-print "\n";
+print STDERR "did 10 runs\naverage runtime: " . $total / $num . "\n";
