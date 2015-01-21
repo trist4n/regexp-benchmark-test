53cf8dc11d8404178e58c3d7dd3dfafd35e282c5
tristan <tristan@ogedai.kgb.im>
2015-01-21 21:44:11 +1100

    some initial results, more versions coming

--- lib/RegexTester.pm
+++ lib/RegexTester.pm
@@ -2,6 +2,7 @@ package RegexTester;
 use strict;
 use warnings;
 use Carp;
+use Regexp::Assemble;
 use LWP::UserAgent;
 use PerlIO::gzip;
 use feature 'state';
@@ -14,6 +15,26 @@ sub openPath {
 	open(my $fh, "<:gzip", $path) || croak($!);
 	return $fh;
 }
+
+sub fhToAsRegexp {
+	my $fh = shift;
+	warn "assembling into giant or regexp";
+
+	my $ra = Regexp::Assemble->new;
+	my $c;
+	while(my $line = <$fh>) {
+		if( (++$c % 100_000) == 0) {
+			print STDERR localtime . " processed: $c\r";
+		}
+		chomp($line);
+		if(!acceptLine($line)) {
+			next;
+		}
+		$ra->add(quotemeta(lc($line)));
+	}
+
+	return $ra->re;
+} 
 
 sub fhToRegexp {
 	my $fh = shift;
